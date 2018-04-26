c04018ba0bfc50c3dcba31627bde1a6b07f48d91
luisenktt <kttweb@gmail.com>
2018-04-26 11:45:06 +0200

    Usuario y token listo

---
+++ models/usuario.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+var uniqueValidator = require('mongoose-unique-validator');
+
+var Schema = mongoose.Schema;
+
+// Para controlar los roles que están permitidos
+var rolesValidos = {
+    values: ['ADMIN_ROLE', 'USER_ROLE'],
+    message: '{VALUE} no es un role valido'
+}
+
+var usuarioSchema = new Schema({
+    nombre: { type: String, required: [true, 'El nombre es necesario'], },
+    email: { type: String, unique: true, required: [true, 'El correo es necesario'], },
+    password: { type: String, required: [true, 'La contraseña es necesaria'] },
+    img: { type: String, required: false },
+    role: { type: String, required: true, default: 'USER_ROLE', enum: rolesValidos }
+});
+
+// {PATH} -> par indicar en caso de que el campo tenga la propiedad unique
+usuarioSchema.plugin(uniqueValidator, { message: '{PATH} debe de ser único' });
+
+module.exports = mongoose.model('Usuario', usuarioSchema);
